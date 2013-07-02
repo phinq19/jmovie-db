2a1b0ece086c7c33e8c8c21dbabe36e8b3cb2297
lars.albrecht@gmail.com <lars.albrecht@gmail.com@24d9ebd2-424c-fbbb-f2ba-0ac280511860>
2013-07-02 12:50:39 +0000

    -> added test -> moved TestUtility to test -> added some more configurable values for FileDetailsPage

--- trunk/src/com/lars_albrecht/mdb/main/core/interfaces/web/pages/abstracts/AbstractFileDetailsOutputItem.java
+++ trunk/src/com/lars_albrecht/mdb/main/core/interfaces/web/pages/abstracts/AbstractFileDetailsOutputItem.java
@@ -3,15 +3,18 @@
  */
 package com.lars_albrecht.mdb.main.core.interfaces.web.pages.abstracts;
 
+import com.lars_albrecht.mdb.main.core.models.Key;
 import com.lars_albrecht.mdb.main.core.models.KeyValue;
 
 /**
  * @author lalbrecht
  * 
+ *         Use this abstract to modify the output of the FileDetailsPage.
+ * 
  */
 public abstract class AbstractFileDetailsOutputItem {
 
-	protected final String getDefault(final String infoType,
+	protected final String getDefaultValue(final String infoType,
 			final String sectionName,
 			final KeyValue<String, Object> keyValue,
 			final String value) {
@@ -22,5 +25,13 @@ public abstract class AbstractFileDetailsOutputItem {
 			final String sectionName,
 			final KeyValue<String, Object> keyValue,
 			final String value);
+
+	public abstract boolean keyAllowed(final Key<String> key);
+
+	protected final String getDefaultKey(final String infoType, final String sectionName, final KeyValue<String, Object> keyValue) {
+		return keyValue.getKey().getKey();
+	}
+
+	public abstract String getKey(final String infoType, final String sectionName, final KeyValue<String, Object> keyValue);
 
 }
